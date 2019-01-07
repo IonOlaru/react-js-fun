cddc027f86b782c442e20af34ef33349ca37c9af
iolaru <iolaru>
2019-01-07 15:48:29 -0500

    form

--- mosh-vidly/src/components/movieForm.jsx
+++ mosh-vidly/src/components/movieForm.jsx
@@ -1,7 +1,21 @@
-import React, { Component } from "react";
+import React, { ReactFragment, Component } from "react";
 
-const MovieForm = () => {
-  return <h1>MovieForm</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <React.Fragment>
+      <h1>MovieForm</h1>
+      <div>
+        {match.params.id}
+        <br />
+        <button
+          className="btn btn-primary"
+          onClick={() => history.push("/movies")}
+        >
+          Save
+        </button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default MovieForm;
--- mosh-vidly/src/components/navBar.jsx
+++ mosh-vidly/src/components/navBar.jsx
@@ -20,6 +20,7 @@ const NavBar = () => {
       >
         <span className="navbar-toggler-icon" />
       </button>
+
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
