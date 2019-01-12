649f55f2241deebafe3e6a87764cf6e93d396e62
Ion Olaru <ion@olaru.us>
2019-01-12 10:30:12 -0500

    show deleteColumn for admin user only

--- mosh-vidly/src/components/moviesTable.jsx
+++ mosh-vidly/src/components/moviesTable.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import auth from "../services/authService";
 import Like from "./common/like";
 import Table from "./common/table";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import authService from "../services/authService";
 
 class MoviesTable extends Component {
   columns = [
     { key: "indexColumn", label: "#" },
-    { path: "title", label: "Title", content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
+    {
+      path: "title",
+      label: "Title",
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
@@ -15,19 +21,26 @@ class MoviesTable extends Component {
       content: movie => (
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
-    },
-    {
-      key: "delete",
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      )
     }
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    )
+  };
+
+  constructor() {
+    super();
+    const user = authService.getCurrentUser();
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
 
   render() {
     const { movies, sortColumn, onSort, currentPage, pageSize } = this.props;
